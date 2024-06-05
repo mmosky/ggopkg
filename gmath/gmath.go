2c8699e29d6810cfd9a9daf037852b229b6b1624
mosky <q94354906@gmail.com>
2024-06-05 23:00:51 +0800

    feat: gmath: add some func

---
+++ gmath/gmath.go
@@ -0,0 +1,57 @@
+package gmath
+
+import (
+	"github.com/mmosky/ggopkg/constraint"
+	"github.com/mmosky/ggopkg/gval"
+)
+
+func Abs[T constraint.Number](x T) T {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+// Max returns the maximum value in the slice x. If the slice is empty, Max
+// returns the zero value of T.
+func Max[T constraint.Ordered](x ...T) T {
+	if len(x) == 0 {
+		return gval.Zero[T]()
+	}
+	max := x[0]
+	for _, v := range x[1:] {
+		if v > max {
+			max = v
+		}
+	}
+	return max
+}
+
+// Min returns the minimum value in the slice x. If the slice is empty, Min
+// returns the zero value of T.
+func Min[T constraint.Ordered](x ...T) T {
+	if len(x) == 0 {
+		return gval.Zero[T]()
+	}
+	min := x[0]
+	for _, v := range x[1:] {
+		if v < min {
+			min = v
+		}
+	}
+	return min
+}
+
+func Sum[T constraint.Number](x ...T) T {
+	var sum T
+	for _, v := range x {
+		sum += v
+	}
+	return sum
+}
+
+// Avg returns the average of the values in the slice x. If the slice is empty,
+// Avg panics.
+func Avg[T constraint.Number](x ...T) T {
+	return Sum(x...) / T(len(x))
+}
